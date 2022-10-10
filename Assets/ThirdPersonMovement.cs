634d4048f593a487d235fe00036673edcbb98b9a
hiyjokou <hiyjokou@gmail.com>
2022-10-10 10:03:39 +0200

    added gravity without kinetic body

--- Assets/ThirdPersonMovement.cs
+++ Assets/ThirdPersonMovement.cs
@@ -8,12 +8,25 @@ public class ThirdPersonMovement : MonoBehaviour{
     public Transform cam;
 
     public float speed = 6f;
-
+    public float gravity = -9.81f;
     public float turnSmoothTime = 0.1f;
+    
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
     float turnSmoothVelocity;
 
 
     void Update(){
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0){
+            velocity.y = -2f;
+        }
 
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
@@ -26,8 +39,10 @@ public class ThirdPersonMovement : MonoBehaviour{
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir.normalized * speed * Time.deltaTime);
-           
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);           
         }
+        
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 }
