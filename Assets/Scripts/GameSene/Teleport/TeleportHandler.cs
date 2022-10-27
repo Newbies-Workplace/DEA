d71f96cbd7f56f5a3642218b7486479e12abfaff
hiyjokou <hiyjokou@gmail.com>
2022-10-27 19:44:14 +0200

    map into game scene

---
+++ Assets/Scripts/GameSene/Teleport/TeleportHandler.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class TeleportHandler : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject Cinemachine;
+    [SerializeField] private GameObject parter;
+    [SerializeField] private GameObject pietro;
+    [SerializeField] private static bool isWhere = false; //true == parter , false == pietro
+
+    void Start(){
+        player = GameObject.Find("ThirdPersonPlayer");
+    }
+
+    private void OnTriggerEnter(Collider other){
+        if(other.tag == "Player"){
+            if(isWhere){
+
+                player.GetComponent<Transform>().position = parter.transform.position;
+                Cinemachine.GetComponent<CinemachineFreeLook>().ForceCameraPosition(player.transform.position, Quaternion.Euler(150, 0, 0));  
+                isWhere = !isWhere;
+            }
+            else{
+
+                player.GetComponent<Transform>().position = pietro.transform.position;
+                Cinemachine.GetComponent<CinemachineFreeLook>().ForceCameraPosition(player.transform.position, Quaternion.Euler(0, 0, 0));
+                isWhere = !isWhere;
+            }
+        }
+    }
+}
